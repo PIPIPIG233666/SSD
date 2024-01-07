61b0136defc9692d2b1653ee702eda43ddb4b472
pppig236 <priv@pppig236.com>
2024-01-07 10:30:19 -0500

    initial update for converting data into arrays

--- processNames.sh
+++ processNames.sh
@@ -6,10 +6,10 @@ ssd=ssd.csv
 sed -i 's/kioxia/KIOXIA/gI' $ssd
 sed -i 's/intel/INTEL/gI' $ssd
 sed -i 's/fanxiang/FANXIANG/gI' $ssd
-sed -i "s/'SK/'SK Hynix/gI" $ssd
-sed -i "s/'P41 P/'Solidigm P41 P/gI" $ssd
-sed -i "s/'PM/'Samsung PM/gI" $ssd
-sed -i "s/'PC801/'SK Hynix PC801/gI" $ssd
+sed -i "s/SK/SK Hynix/gI" $ssd
+sed -i "s/P41 P/Solidigm P41 P/gI" $ssd
+sed -i "s/,PM/,Samsung PM/gI" $ssd
+sed -i "s/PC801/SK Hynix PC801/gI" $ssd
 sed -i "s/memblaze/MemBlaze/gI" $ssd
 sed -i "s/容量过低没办法测/NaN/g" $ssd
 sed -i 's/"发生错误,跑不了"/NaN/g' $ssd
@@ -19,3 +19,5 @@ sed -i "s/大容量DC不重复测试/NaN/g" $ssd
 sed -i "s/大容量DC不重复测试/NaN/g" $ssd
 sed -i "s/测试错误,跑不完/NaN/g" $ssd
 sed -i "s/没测/NaN/g" $ssd
+sed -i "s/'NaN'/NaN/g" $ssd
+sed -i 's/"NaN"/NaN/g' $ssd
