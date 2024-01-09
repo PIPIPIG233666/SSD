b0f38e52ed1f4481d7bf33873daf6f38421c3b96
pppig236 <priv@pppig236.com>
2024-01-09 12:03:46 -0500

    move csv to assets
    Signed-off-by: pppig236 <priv@pppig236.com>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
 import vue from '@vitejs/plugin-vue'
+import dsv from '@rollup/plugin-dsv'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    // dsv should skip empty lines
+    dsv({ skipEmptyLines: true })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
